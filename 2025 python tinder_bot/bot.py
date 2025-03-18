a052c1dded3122b6b24c455573885a6fc467323d
JARVIS-VOVA <jarvis.vova@gmail.com>
2025-03-18 20:54:33 +0200

    Lesson #1 Add handlers for /start. Reply after other text Also sent in chat text, buttons and photos

--- 2025 python tinder_bot/bot.py
+++ 2025 python tinder_bot/bot.py
@@ -3,8 +3,30 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQu
 from gpt import *
 from util import *
 
-# тут будемо писати наш код :)
+TELEGRAM_BOT_TOKEN = "8034267?49:AAEw_HNWPHE6BoU_Jj2?Wx8QGnk0Geq-vMY"
 
+async def start(update, context):
+    # await send_photo(update, context, "avatar_main")
+    # await send_text(update, context, "привіт користувач")
+    message = load_message("main")
+    await send_text(update, context, message)
 
-app = ApplicationBuilder().token("telegram-token").build()
+async def hello(update, context):
+    # await send_text(update, context, "Hello" + update.message.text)
+    await send_text_buttons(update, context, "Hello " + update.message.text, {
+        "start": "Start",
+        "stop": "Stop"
+    })
+
+async def buttons_handler(update, context):
+    query = update.callback_query.data
+    if query == "start":
+        await send_text(update, context, "Started")
+    elif query == "stop":
+        await send_text(update, context, "Stopped")
+
+app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+app.add_handler(CallbackQueryHandler(buttons_handler))
 app.run_polling()
